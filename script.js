3e2ab4c58c891883445e7e72f77b11faa27525f9
Thawan Silva <84919334+thawansilva@users.noreply.github.com>
2022-02-21 15:24:59 -0300

    Update script.js
    Adicionei mais funções para fazer a animação ao scroll das seções

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict"
-
+// Animação Scroll suave
 const navItems = document.querySelectorAll('.nav_list a[href^="#"]')
 navItems.forEach(item => {
     item.addEventListener('click', scroolToElementOnClick)
@@ -22,3 +22,43 @@ function scroolToPosition(y) {
         behavior: 'smooth'
     })
 }
+
+// Animação ao scroll
+
+// Debounce para "segurar" o scroll
+const debounce = function (func, wait, immediate) {
+    let timeout;
+    return function (...args) {
+        const context = this;
+        const later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+// Fazendo o efeito de aparecimento ao Scroll
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScrool() {
+    const windowTop = window.scrollY + (window.innerHeight * .85);
+    target.forEach(element => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add(animationClass);
+        } else {
+            element.classList.remove(animationClass);
+        }
+    })
+}
+animeScrool();
+
+if (target.length) {
+    window.addEventListener('scroll', debounce(function () {
+        animeScrool();
+    }, 20))
+};
